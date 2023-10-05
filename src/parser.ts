4031e00f9ae13ee6e650b2613b2b87cd65b8c5ff
Sepehr <62986692+IIIphr@users.noreply.github.com>
2023-10-05 17:19:58 +0330

    Bug fixes on parser. Still needs work for accurate token parsing

--- src/parser.ts
+++ src/parser.ts
@@ -4,13 +4,11 @@ export class Parser {
 
     static specialChars = ['.', ',', '{', '}', ';']
 
-    static parse(document: vscode.TextDocument) : string[] {
+    static parse(document: vscode.TextDocument): string[] {
         var res = [];
-        for (var line = 1; line <= document.lineCount; line++){
-            const lineText = document.lineAt(line);
-            const words = lineText.text.trim().split(' ');
+        for (var line = 0; line < document.lineCount; line++) {
+            const words = document.lineAt(line).text.trim().split(" ");
             res.push(...words);
-            console.log("res: " + JSON.stringify(res));
         }
         return res;
     }
