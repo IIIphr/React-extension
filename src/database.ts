5b85b9d2eeea9fa17b51d605152a0705dde232fe
IIIphr <sepehr.ebi.1380@gmail.com>
2023-11-30 18:07:03 +0330

    Added 'useState' guide, and prism for code styling

--- src/database.ts
+++ src/database.ts
@@ -14,11 +14,11 @@ export class Database {
 
     static encode(context: vscode.ExtensionContext){
         const encoded = Buffer.from(JSON.stringify(db)).toString('base64');
-        writeFileSync(context.extensionPath.toString() + "/src/encoded.db", encoded);
+        writeFileSync(context.extensionPath + "/src/encoded.db", encoded);
     }
 
     static decode(context: vscode.ExtensionContext){
         const decoded = Buffer.from(readFileSync(context.extensionPath.toString() + "/src/encoded.db").toString(), 'base64').toString('utf-8');
-        writeFileSync(context.extensionPath.toString() + "/src/decoded.json", decoded.toString());
+        writeFileSync(context.extensionPath + "/src/decoded.json", decoded.toString());
     }
 }
--- src/panelBuilder.ts
+++ src/panelBuilder.ts
@@ -4,17 +4,37 @@ import * as fs from 'fs';
 export class PanelBuilder {
 
     static buildPanel(title: string, header: string, body: string, context: vscode.ExtensionContext) {
-        fs.readFile(context.extensionPath + '/src/style.css', (err, data) => {
+        fs.readFile(context.extensionPath + '/src/style.css', (err, styleData) => {
             if(err){
                 console.log(err);
             }
             else{
-                const style = data.toString();
-                const panel = vscode.window.createWebviewPanel('guide', title, vscode.ViewColumn.Beside);
-                panel.webview.html = 
-                "<style>" + style + "</style>" +
-                "<h1>" + header + "</h1>" +
-                body;
+                fs.readFile(context.extensionPath + '/src/prism.css', (err, prismData) => {
+                    if(err){
+                        console.log(err);
+                    }
+                    else{
+                        fs.readFile(context.extensionPath + '/src/prism.js', (err, jsData) => {
+                            if(err){
+                                console.log(err);
+                            }
+                            else{
+                                const style = styleData.toString();
+                                const prism = prismData.toString();
+                                const js = jsData.toString();
+                                const panel = vscode.window.createWebviewPanel('guide', title, vscode.ViewColumn.Beside, {
+                                    enableScripts: true
+                                });
+                                panel.webview.html = 
+                                "<style>" + prism + "</style>" +
+                                "<style>" + style + "</style>" +
+                                "<h1>" + header + "</h1>" +
+                                body +
+                                "<script>" + js + "</script>";
+                            }
+                        });
+                    }
+                });
             }
         });
     }
