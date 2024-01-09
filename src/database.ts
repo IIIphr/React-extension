3f8b6c76f8cc6852ca08345c5505bfa01006bb68
IIIphr <sepehr.ebi.1380@gmail.com>
2024-01-09 16:42:26 +0330

    Added RTL support for guides

--- src/database.ts
+++ src/database.ts
@@ -7,7 +7,22 @@ export class Database {
 
     static md = markdownit("commonmark", {
         html: true
-    });
+    }).use(
+        require('markdown-it-container'), 'rtl', {
+            // render: function (tokens, idx){
+            //     var m = tokens[idx].info.trim().match(/^rtl\s+(.*)$/);
+
+            //     if (tokens[idx].nesting === 1) {
+            //       // opening tag
+            //       return '<div class>\n';
+            
+            //     } else {
+            //       // closing tag
+            //       return '</details>\n';
+            //     }
+            // }
+        }
+    );
 
     static getGuide(word: string, context: vscode.ExtensionContext): string{
         const index = db.guides.findIndex(e => e.word === word);
