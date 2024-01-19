608812229167248ee1801323195cfc024c1bdecc
IIIphr <sepehr.ebi.1380@gmail.com>
2024-01-19 11:29:19 +0330

    Better logic for importing js and css files

--- src/panelBuilder.ts
+++ src/panelBuilder.ts
@@ -3,38 +3,46 @@ import * as fs from 'fs';
 
 export class PanelBuilder {
 
+    static styleData: string | null = null;
+    static prismCss: string | null = null;
+    static prismJs: string | null = null;
+
+    static getStyleData(context: vscode.ExtensionContext) {
+        this.styleData = fs.readFileSync(context.extensionPath + '/src/style.css').toString();
+
+    }
+
+    static getPrismCss(context: vscode.ExtensionContext) {
+        this.prismCss = fs.readFileSync(context.extensionPath + '/src/prism.css').toString();
+
+    }
+
+    static getPrismJs(context: vscode.ExtensionContext) {
+        this.prismJs = fs.readFileSync(context.extensionPath + '/src/prism.js').toString();
+    }
+
+    static dataCheck(context: vscode.ExtensionContext) {
+        if (this.styleData === null) {
+            this.getStyleData(context);
+        }
+        if (this.prismCss === null) {
+            this.getPrismCss(context);
+        }
+        if (this.prismJs === null) {
+            this.getPrismJs(context);
+        }
+    }
+
     static buildPanel(title: string, body: string, context: vscode.ExtensionContext) {
-        fs.readFile(context.extensionPath + '/src/style.css', (err, styleData) => {
-            if(err){
-                console.log(err);
-            }
-            else{
-                fs.readFile(context.extensionPath + '/src/prism.css', (err, prismData) => {
-                    if(err){
-                        console.log(err);
-                    }
-                    else{
-                        fs.readFile(context.extensionPath + '/src/prism.js', (err, jsData) => {
-                            if(err){
-                                console.log(err);
-                            }
-                            else{
-                                const style = styleData.toString();
-                                const prism = prismData.toString();
-                                const js = jsData.toString();
-                                const panel = vscode.window.createWebviewPanel('guide', title, vscode.ViewColumn.Beside, {
-                                    enableScripts: true
-                                });
-                                panel.webview.html = 
-                                "<style>" + prism + "</style>\n" +
-                                "<style>" + style + "</style>\n" +
-                                body +
-                                "\n<script>" + js + "</script>";
-                            }
-                        });
-                    }
-                });
-            }
+        this.dataCheck(context);
+
+        const panel = vscode.window.createWebviewPanel('guide', title, vscode.ViewColumn.Beside, {
+            enableScripts: true
         });
+        panel.webview.html = 
+        "<style>" + this.prismCss + "</style>\n" +
+        "<style>" + this.styleData + "</style>\n" +
+        body +
+        "\n<script>" + this.prismJs + "</script>";
     }
 }
